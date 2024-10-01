7310ec07b848978d007d4ca9d481b2de1d94136f
Michael Jones <jonesmichael3.1415@gmail.com>
2024-10-01 09:38:12 -0400

    Better build script from Sunip

--- run_build.zsh
+++ run_build.zsh
@@ -1,9 +1,13 @@
+#!/usr/bin/env zsh
+CMAKE_PREFIX_PATH=${PWD}/libraries/libtorch/share/cmake/Torch;
 rm -rf build;
 mkdir build;
 cd build;
+echo "Script running in: ${PWD}"
+echo "Cmake Prefix: ${CMAKE_PREFIX_PATH}"
 echo "*\n!.gitignore" > .gitignore;
 # Absolute path to the ./libraries/libtorch folder
-cmake -DCMAKE_PREFIX_PATH=/home/michael-jones/Documents/pytorch_model_in_c/libraries/libtorch ..;
+cmake -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} ..;
 cmake --build . --config Release;
 # Run a test to ensure everything works (use only 1 core)
 time taskset --cpu-list 1 ./main ../data_groups_approx_v2_2_epoch238.pt;
